9bff1839b928fc8009cbb8a3f51781b502de5c1f
kaylorchen <kaylor.chen@qq.com>
2025-07-05 17:05:30 +0800

    add easy logger

--- Core/User/user_task_impl.cpp
+++ Core/User/user_task_impl.cpp
@@ -3,12 +3,21 @@
 //
 
 #include "user_task_impl.h"
-#include "main.h"
+
+#include "elog.h"
 #include "interface.h"
+#include "main.h"
 
 void UserTaskImpl::UserMainLoopCallback() {
+  elog_init();
+  elog_start();
   while (1){
-   Interface::get_instance().print("This is a test\r\n");
+    elog_a("UserMainLoop", "assert");
+    elog_e("UserMainLoop", "error");
+    elog_w("UserMainLoop", "warn");
+    elog_i("UserMainLoop", "info");
+    elog_d("UserMainLoop", "debug");
+    elog_v("UserMainLoop", "verbose");
     HAL_Delay(1000);
   }
 }
